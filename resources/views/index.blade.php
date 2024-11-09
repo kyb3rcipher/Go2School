c97fa4869031880904fd8dc7d27e8887aad2b42c
Kyb3r Cipher <me@kyb3rcipher.com>
2024-11-09 12:46:07 -0600

    Create landing index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="images/logo.svg">
+    
+    <!-- Style -->
+    <!-- Boostrap CDN --> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    
+    <!-- Navbar section -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <img src="images/banner.png" alt="logo">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#features">Features</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+                    <li class="nav-item me-5"><a class="nav-link" href="#">About</a></li>
+                    
+                    <!--<li class="nav-item dropdown me-5">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown link</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>-->
+                </ul>
+                <a href="#" class="btn btn-outline-secondary shadow-sm d-sm d-block">Platform</a>
+          </div>
+        </div>
+    </nav>
+    
+    <!-- Hero section -->
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text">Modernize your school and keep better control</div>
+                    <div class="buttons">
+                        <a href="#" class="btn btn-primary">Go to Platform</a>
+                        <a href="#" class="btn btn-outline-secondary ms-3">Test a preview</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="/assets/img/hero-img.svg" alt="hero-image" class="w-100">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Setup section -->
+    <section id="features" class="setup">
+        <div class="container">
+            <div class="text-header text-center">
+                <h3>The best option</h3>
+                <p>We are not just another platform for the control and management of your school. We are not a platform, Go2Trade <strong>IS the platform.</strong></p>
+            </div>
+            <div class="items text-center">
+                <div class="row">
+
+                    <div class="col-md-4">
+                        <div class="icons">
+                            <img src="/assets/img/icons/icons-1.svg" alt="icons">
+                        </div>
+                        <div class="desc">
+                            <h5>Control your students</h5>
+                            <p>Monitor your students' grades, school entrances and exits, behavior reports, student information <strong>and more</strong>.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="icons">
+                            <img src="/assets/img/icons/icons-2.svg" alt="icons">
+                        </div>
+                        <div class="desc">
+                            <h5>Manage your school</h5>
+                            <p>Control your school in a more modern way. Create schedules dynamically for both students and teachers. Control teacher attendance at classes. And manage in a <strong>more efficient way</strong>.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="icons">
+                            <img src="/assets/img/icons/icons-3.svg" alt="icons">
+                        </div>
+                        <div class="desc">
+                            <h5>Help for parents</h5>
+                            <p>Help parents see their children's academic performance digitally in an easy way, help them see their attendance or request excuses for absences in <strong>a simple way</strong>.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Information section -->
+    <!--<section class="information">
+        <div class="container">
+
+            <div class="row info-1">
+                <div class="col md-6">
+                    <div class="text-information">
+                        <h5>Get more costumer for your bussniess</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi impedit at aspernatur eaque! Blanditiis qui cumque unde voluptatum? Adipisci, autem.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="/assets/img/image-1.svg" alt="image-1" class="w-100">
+                </div>
+            </div>
+
+            <div class="row info-2">
+                <div class="col-md-6">
+                    <img src="/assets/img/image-2.svg" alt="image-2" class="w-100">
+                </div>
+                <div class="col-md-6">
+                    <div class="text-information">
+                        <h5>Low budget high quality look profressional</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi impedit at aspernatur eaque! Blanditiis qui cumque unde voluptatum? Adipisci, autem.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>-->
+
+    <!-- Footer section -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col md-6">
+                    <div class="row row-cols-1 row-cols-lg-5 g-2 lg-3">
+
+                        <div class="col-md-3">
+                            <div><small><a href="#" class="text-decoration-none">Home</a></small></div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div><small><a href="#features" class="text-decoration-none">Features</a></small></div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div><small><a href="#" class="text-decoration-none">About</a></small></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="copy">&copy; 2024 Kyb3r Cipher</div>
+        </div>
+    </footer>
+
+
+    <!-- Boostrap CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
