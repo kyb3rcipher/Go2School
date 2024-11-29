80ba1124019559ac8fb6471d5e3da9a157b8c62b
Kyb3r Cipher <me@kyb3rcipher.com>
2024-11-28 23:26:26 -0600

    Create groups index page (table)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\GroupController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'landing-home.index');
@@ -15,6 +16,9 @@ Route::view('/platform', 'platform.index')->name('platform-index');
 Route::resource('/platform/students', StudentController::class)->except(['show']);
 Route::get('/platform/students/list', [StudentController::class, 'list'])->name('students.list');
 Route::get('/platform/students/list/view/{id}', [StudentController::class, 'view'])->name('students.view');
+
+// Groups
+Route::resource('/platform/students/groups', GroupController::class)->name('index', 'students.groups');
 
 // Teachers
 Route::resource('/platform/teachers', TeacherController::class)->except('show');
