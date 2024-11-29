9a0b2a7c74a949f80183469de436e12fdf6cb855
Kyb3r Cipher <me@kyb3rcipher.com>
2024-11-28 18:37:03 -0600

    Add seeders for default carrers Create classrooms migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Student;
 use App\Models\Group;
+use App\Models\Carrer;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,6 +12,16 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $carrers = [
+            ['name' => 'Programation'],
+            ['name' => 'Architecture'],
+            ['name' => 'Electronic'],
+            ['name' => 'ARH'],
+        ];
+        foreach ($carrers as $carrer) {
+            Carrer::create($carrer);
+        }
+
         Group::factory()->create([
             'grade' => '1 - 2',
             'group' => 'A',
