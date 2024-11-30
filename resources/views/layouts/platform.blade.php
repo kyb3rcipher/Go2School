987e6a4ff4878bc2114567fb6eb6083aa1554311
Kyb3r Cipher <me@kyb3rcipher.com>
2024-11-30 10:12:13 -0600

    Add stack dinamic css and js in platform layout

--- resources/views/layouts/platform.blade.php
+++ resources/views/layouts/platform.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="/css/global.css">
     <link rel="stylesheet" href="/css/platform.css">
     <link rel="stylesheet" href="/css/dashboard.css">
+    @stack('css')
 
     <!-- Font Awesome (temporal: change to bootstrap icons) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -310,5 +311,6 @@
 
 @livewireScripts
 <script src="/js/script.js"></script>
+@stack('js')
 </body>
 </html>
