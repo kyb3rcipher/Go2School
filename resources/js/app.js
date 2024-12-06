62b7375afd31de0378640a3497bba262578326e9
Kyb3r Cipher <me@kyb3rcipher.com>
2024-12-05 22:32:51 -0600

    Install bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import '../scss/app.scss';
+import * as bootstrap from 'bootstrap';
+
+// Enable bootstrap tooltips
+document.addEventListener('DOMContentLoaded', () => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+});
