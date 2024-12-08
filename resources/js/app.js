d4b61ccf9b9dbf4c7028dc76837676bd5ad227f0
Kyb3r Cipher <me@kyb3rcipher.com>
2024-12-07 22:41:00 -0600

    Install sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 import './bootstrap';
 import '../scss/app.scss';
 import * as bootstrap from 'bootstrap';
+import Swal from 'sweetalert2';
 
 // Enable bootstrap tooltips
 document.addEventListener('DOMContentLoaded', () => {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
+
+// Make global sweetAlert
+window.Swal = Swal;
